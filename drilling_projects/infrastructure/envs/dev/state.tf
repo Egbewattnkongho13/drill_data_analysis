ff3a48b979b6f4600c44e2c1304ff94418650af8
Egbewatt Nkongho <Kabanajawat@gmail.com>
2025-06-12 17:33:30 +0100

    first commit

---
+++ drilling_projects/infrastructure/envs/dev/state.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "egbewattdevops-state-bucket-dev"
+    key            = "terraform-state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-table"
+    encrypt        = true    
+  }
+}
