c40318be48479eb6744a4c93e6a28ceb4f6d99cc
Egbewatt Nkongho <Kabanajawat@gmail.com>
2025-06-24 22:53:31 +0100

    patch: lint provider.tf

--- infrastructure/envs/dev/provider.tf
+++ infrastructure/envs/dev/provider.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "us-east-1"
-  profile = "default"
+
 
   default_tags {
     tags = {
       Environment  = "dev"
       Owner        = "OyeData"
       project_name = "drill_data_analysis"
-      }
+    }
   }
 }
 
