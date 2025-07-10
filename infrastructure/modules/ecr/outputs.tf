32782dcb1da3c898ac354aba0342761ac6bedd31
Egbewatt Nkongho <Kabanajawat@gmail.com>
2025-07-10 20:30:00 +0100

    chore:  add ECR output description

--- infrastructure/modules/ecr/outputs.tf
+++ infrastructure/modules/ecr/outputs.tf
@@ -1,8 +1,9 @@
 output "repository_url" {
-  value = aws_ecr_repository.ecr_repo.repository_url
-
+  value       = aws_ecr_repository.ecr_repo.repository_url
+  description = "The URL of the ECR repository"
 }
 
 output "arn_of_ecr_repository" {
-  value = aws_ecr_repository.ecr_repo.arn
+  value       = aws_ecr_repository.ecr_repo.arn
+  description = "The ARN of the ECR repository"
 }
