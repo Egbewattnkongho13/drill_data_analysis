2282e557aef30ec4b76ec621bfe53bd0d145c601
Egbewatt Nkongho <Kabanajawat@gmail.com>
2025-07-11 20:09:15 +0100

    fix(terraform): Add variable validation for lambdas input

--- infrastructure/envs/dev/variables.tf
+++ infrastructure/envs/dev/variables.tf
@@ -1,14 +1,29 @@
 variable "ingestion_docker_image_tag" {
   type        = string
   description = "Docker image tag for the ingestion lambda"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9._-]+$", var.ingestion_docker_image_tag))
+    error_message = "The ingestion_docker_image_tag must consist of alphanumeric characters, dots, underscores, or hyphens."
+  }
 }
 
 variable "silver_docker_image_tag" {
   type        = string
   description = "Docker image tag for the silver transform lambda"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9._-]+$", var.silver_docker_image_tag))
+    error_message = "The silver_docker_image_tag must consist of alphanumeric characters, dots, underscores, or hyphens."
+  }
 }
 
 variable "gold_docker_image_tag" {
   type        = string
   description = "Docker image tag for the gold transform lambda"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9._-]+$", var.gold_docker_image_tag))
+    error_message = "The gold_docker_image_tag must consist of alphanumeric characters, dots, underscores, or hyphens."
+  }
 }
