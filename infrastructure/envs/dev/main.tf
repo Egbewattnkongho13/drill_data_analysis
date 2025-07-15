e7b35003513bae88d5ce8b5999df2eaf15a5e342
Egbewatt Nkongho <Kabanajawat@gmail.com>
2025-07-15 15:42:19 +0100

    style: format terraform main.tf file

--- infrastructure/envs/dev/main.tf
+++ infrastructure/envs/dev/main.tf
@@ -53,12 +53,12 @@ module "Gold_transform_lambada" {
 module "DataLake" {
   source = "../../modules/datalake"
 
-  datalake_name                 = "oye-data-lake"
-  account_id                    = data.aws_caller_identity.current.account_id
-  ingestion_lambda_arn          = module.ingestion_lambada.arn_of_lambda_function
-  silver_transform_lambda_arn   = module.Silver_transform_lambada.arn_of_lambda_function
-  gold_transform_lambda_arn  = module.Gold_transform_lambada.arn_of_lambda_function
-  region                        = var.region
+  datalake_name               = "oye-data-lake"
+  account_id                  = data.aws_caller_identity.current.account_id
+  ingestion_lambda_arn        = module.ingestion_lambada.arn_of_lambda_function
+  silver_transform_lambda_arn = module.Silver_transform_lambada.arn_of_lambda_function
+  gold_transform_lambda_arn   = module.Gold_transform_lambada.arn_of_lambda_function
+  region                      = var.region
 
   depends_on = [
     module.ingestion_lambada,
