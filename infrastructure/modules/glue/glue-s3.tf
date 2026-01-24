9872f26699fcfdd92ce1a1377ff446f4515e948f
Egbewatt Nkongho <Kabanajawat@gmail.com>
2026-01-24 13:06:38 +0100

    fix(glue-terraform): Enable cloudwatch logging configuration

--- infrastructure/modules/glue/glue-s3.tf
+++ infrastructure/modules/glue/glue-s3.tf
@@ -1,3 +1,7 @@
+resource "aws_cloudwatch_log_group" "ingestion-lg" {
+  name              = "/glue/${var.glue_job_name}"
+  retention_in_days = 14
+}
 resource "aws_glue_job" "this" {
   name         = var.glue_job_name
   role_arn     = aws_iam_role.glue_job_role.arn
@@ -9,6 +13,9 @@ resource "aws_glue_job" "this" {
     "--job-language"             = "python"
     "--enable-glue-datacatalog"  = ""
     "--pip-install"              = "s3://${aws_s3_bucket.glue_assets.id}/${aws_s3_object.glue_job_wheel.key}"
+    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.ingestion-lg.name
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"     = "true"
   }
 
   command {
