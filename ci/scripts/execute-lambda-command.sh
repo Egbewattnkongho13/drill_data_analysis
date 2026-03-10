063485e5eeba69bfbb445e64330ee851e69b2ac1
Egbewatt Nkongho <Kabanajawat@gmail.com>
2026-03-10 12:37:43 +0100

    fix(ci-scripts): Correct directory context for execute-lambda-command.sh

--- ci/scripts/execute-lambda-command.sh
+++ ci/scripts/execute-lambda-command.sh
@@ -14,28 +14,19 @@ LAMBDAS_DIR="lambdas"
 BUILD_LAMBDA=$1
 CMD="${@:2}"
 
+echo "PWD: $(pwd)"
+
 clean_up(){
     # Ensure we return to the original directory
-    popd || exit 1
+    popd >/dev/null 2>&1 || true
 }
 
 trap clean_up EXIT
 
 execute(){
     # Execute the command in the lambda directory
-    if [ -d "$LAMBDAS_DIR/$BUILD_LAMBDA" ]; then
-        echo "Executing command in $LAMBDAS_DIR/$BUILD_LAMBDA"
-
-        pushd "$LAMBDAS_DIR/$BUILD_LAMBDA" || {
-            echo "Error: Could not change directory to $LAMBDAS_DIR/$BUILD_LAMBDA"
-            exit 1
-        }
-
-        $CMD || {
-                echo "Error: Command failed in $LAMBDAS_DIR/$BUILD_LAMBDA"
-                exit 1
-            }
-    fi
-    
+       echo "Executing command in $(pwd)"
+    $CMD
+            
 }
 execute
