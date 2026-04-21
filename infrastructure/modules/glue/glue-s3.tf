64c382eb288978a0a5dce1bcfa5078afbfbf3b43
Egbewatt Nkongho <Kabanajawat@gmail.com>
2026-04-21 10:32:50 +0100

    feat(terraform): Configure Glue job to load all wheels

--- infrastructure/modules/glue/glue-s3.tf
+++ infrastructure/modules/glue/glue-s3.tf
@@ -12,10 +12,10 @@ resource "aws_glue_job" "this" {
   default_arguments = {
     "--job-language"             = "python"
     "--ENVIRONMENT"              = var.environment
-    "--enable-metrics"           = "true"      
+    "--enable-metrics"           = "true"
     "--TempDir"                  = "s3://${aws_s3_bucket.glue_assets.id}/temp/"
     "--enable-glue-datacatalog"  = ""
-    "--extra-py-files"           = "s3://${aws_s3_bucket.glue_assets.id}/${aws_s3_object.glue_job_wheel.key}"
+    "--extra-py-files"           = join(",", [for wheel in aws_s3_object.glue_job_wheels : "s3://${aws_s3_bucket.glue_assets.id}/${wheel.key}"])
     "--additional-python-modules" = "omegaconf==2.3.0,pyyaml==6.0.1,requests==2.28.0,pydantic==2.11.7,kaggle==1.5.12,boto3==1.39.15"
     "--continuous-log-logGroup"  = aws_cloudwatch_log_group.ingestion-lg.name
     "--enable-continuous-cloudwatch-log" = "true"
@@ -34,6 +34,6 @@ resource "aws_glue_job" "this" {
 
   depends_on = [
     aws_s3_object.glue_job_script,
-    aws_s3_object.glue_job_wheel,
+    aws_s3_object.glue_job_wheels,
   ]
 }
